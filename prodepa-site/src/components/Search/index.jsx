ffa520d821fcc96b44bf7c3b0af06de538aa2a60
Yuri Sarmanho <yuri.sarmanho@hotmail.com>
2022-12-21 14:29:25 -0300

    first config of Search component

---
+++ prodepa-site/src/components/Search/index.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import './index.css'
+
+function SearchInput() {
+  return (
+    <>
+    <input className='searchInput' typeof='text' placeholder='pesquisar...'/>
+    
+    </>
+  )
+}
+
+export default SearchInput
--- prodepa-site/src/sections/Header/MiddleHeader/index.jsx
+++ prodepa-site/src/sections/Header/MiddleHeader/index.jsx
@@ -1,10 +1,20 @@
-import React from 'react'
-import './index.css'
+import React from "react";
+import LogoProdepa from "../../../img/logoProdepa2022_0_0.png";
+import SearchInput from '../../../components/Search'
+import "./index.css";
 
 function MiddleHeader() {
   return (
-    <div className='middleHeaderContainer'>MiddleHeader</div>
-  )
+    <div className="middleHeaderContainer">
+      <div className="leftArea-middleHeader">
+        <img src={LogoProdepa} alt="Logo da Prodepa" />
+      </div>
+      <div className="middleArea-middleHeader">
+        <SearchInput/>
+      </div>
+      <div className="rightArea-middleHeader"></div>
+    </div>
+  );
 }
 
-export default MiddleHeader
+export default MiddleHeader;
